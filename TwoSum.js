1cf687c2c2c56aaad3a271929717cc4ff5bd6ca9
Arafin Mridul <mridularafin@gmail.com>
2024-03-28 04:46:47 +0600

    init

---
+++ TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let numMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (complement in numMap) {
+            return [numMap[complement], i];
+        }
+        numMap[nums[i]] = i;
+    }
+};

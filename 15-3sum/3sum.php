a26a32aa6df13f654c9b544680905eb063d67ce1
Arafin Mridul <110626481+arafinmridul@users.noreply.github.com>
2024-03-30 02:03:25 +0600

    Time: 281 ms (50.60%) | Memory: 26.9 MB (90.36%) - LeetSync

--- 15-3sum/3sum.php
+++ 15-3sum/3sum.php
@@ -9,20 +9,23 @@ class Solution {
         sort($nums);
         $ans = array();
         for($i=0; $i<$n; ++$i){
+            if($i > 0 && $nums[$i] == $nums[$i - 1]) continue;
             $to_get = -1 * $nums[$i];
             $l = $i+1; $r = $n-1;
-            while($l < $r){
-                if($nums[$l]+$nums[$r] == $to_get){
+            while($l < $r) {
+                if($nums[$l] + $nums[$r] == $to_get) {
                     array_push($ans, [$nums[$i], $nums[$l], $nums[$r]]);
                     $l++;
+                    // Skip duplicate values
+                    while($l < $r && $nums[$l] == $nums[$l - 1]) $l++;
                 }
-                if($nums[$l] + $nums[$r] < $to_get)
-                    ++$l;
-                else --$r;
+                if($nums[$l] + $nums[$r] < $to_get) {
+                    $l++;
+                } else {
+                    $r--;
+                }
             }
         }
-        $uniqueAns = array_map('unserialize', array_unique(array_map('serialize', $ans)));
-        return $uniqueAns;
-        // return $ans;
+        return $ans;
     }
 }

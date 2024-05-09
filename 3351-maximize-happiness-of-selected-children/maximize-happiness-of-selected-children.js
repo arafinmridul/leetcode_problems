c8fad9b7252572aea4e14f6c071aa0d76065b41e
Arafin Mridul <110626481+arafinmridul@users.noreply.github.com>
2024-05-09 12:37:49 +0600

    Time: 252 ms (30.14%) | Memory: 67.8 MB (49.77%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
@@ -7,8 +7,8 @@ var maximumHappinessSum = function(happiness, k) {
     happiness.sort((a,b)=> b-a);
     let ans = 0;
     for(let i=0; i<k; ++i){
-        if(happiness[i] > i)
-            ans += happiness[i] - i;
+        if(happiness[i] < i) break;
+        ans += happiness[i] - i;
     }
     return ans;
 };

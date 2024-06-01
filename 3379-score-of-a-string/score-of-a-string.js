6d2e10681c52139875364643f64d0780c738ae79
Arafin Mridul <110626481+arafinmridul@users.noreply.github.com>
2024-06-01 15:40:56 +0600

    Time: 62 ms (50.92%) | Memory: 49.5 MB (87.84%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let ans = 0;
+    for(let i=1; i<s.length; ++i)
+        ans += Math.abs(s.charCodeAt(i)-s.charCodeAt(i-1));
+    return ans;
+};

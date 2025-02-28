6179a6a3bc23f33c0fea1e3b4464a68dec2ae80b
Arafin Mridul <110626481+arafinmridul@users.noreply.github.com>
2025-03-01 03:52:20 +0600

    Time: 0 ms (100.00%) | Memory: 35.8 MB (51.55%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -3,9 +3,7 @@ public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
         int left = 0;
-        int sum = 0;
-        for(int i=0; i<n; ++i)
-            sum += nums[i];
+        int sum = reduce(nums.begin(), nums.end(), 0);
 
         for(int i=0; i<n; ++i){
             sum -= nums[i];
